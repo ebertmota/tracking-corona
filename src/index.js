4ec1bd231315b3eb9bb51747464aee71d026f566
ebertmota <ebert.mota03@gmail.com>
2020-04-21 16:04:10 -0300

    Revert "call for hideNavigationBar of react-native-navigation-bar-color"
    This reverts commit de99d9eac148184c4a001e84d697463e7dda5a93.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import './config/ReactotronConfig';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
-import { hideNavigationBar } from 'react-native-navigation-bar-color';
+
 import Routes from './routes';
 
 export default function App() {
-  hideNavigationBar();
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#C04848" />
