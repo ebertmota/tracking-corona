02b752be28d80055b35256a11db2406b4d59714d
ebertmota <ebert.mota03@gmail.com>
2020-04-21 16:08:34 -0300

    adding loader spinner

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components/native';
+import { ActivityIndicator } from 'react-native';
+
 import { WebView } from 'react-native-webview';
 
 export const Browser = styled(WebView)`
+  justify-content: center;
+  align-items: center;
   flex: 1;
+`;
+
+export const Spinner = styled(ActivityIndicator)`
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
 `;
